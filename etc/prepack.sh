5c6c3aa4af303ac6ef441bbccc72c6bb0cca473b
Kaz Nishimura <kazssym@linuxfront.com>
2020-04-27 14:46:41 +0900

    Rename variable 'distdir' to 'outputdir'

--- etc/prepack.sh
+++ etc/prepack.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-distdir="`pwd`"/deploy
+outputdir="`pwd`"/deploy
 
 PACKAGE_NAME="${npm_package_name:-redirect.js}"
 PACKAGE_VERSION="${npm_package_version:-(unversioned)}"
 
 SCRIPTS="redirect.js"
 
-rm -rf "$distdir" || exit 1
-mkdir -p "$distdir" || exit 1
+rm -rf "$outputdir" || exit 1
+mkdir -p "$outputdir" || exit 1
 
 for file in $SCRIPTS; do
     distfile=${file##*/}
     sed \
         -e "s,[@]PACKAGE_NAME[@],$PACKAGE_NAME,g" \
         -e "s,[@]PACKAGE_VERSION[@],$PACKAGE_VERSION,g" \
-        $file > "$distdir"/$distfile || exit 1
+        $file > "$outputdir"/$distfile || exit 1
     "`npm bin`"/terser \
-        "$distdir"/$distfile \
-        --module --output "$distdir"/${distfile%.js}.min.js || exit 1
+        "$outputdir"/$distfile \
+        --module --output "$outputdir"/${distfile%.js}.min.js || exit 1
 done
